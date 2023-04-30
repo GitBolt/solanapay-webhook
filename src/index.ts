97ce7d4de9d8295e523e7fbb92ce941b76618821
GitBolt <saabisakhtar@gmail.com>
2023-04-30 22:47:29 +0530

    add: api protection, improve: code

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { router as webhookRouter } from './routes/webhook'
+import { router as userRouter } from './routes/user'
 
 dotenv.config()
 
@@ -12,6 +13,7 @@ const port = process.env.PORT || '8000';
 app.use(cors())
 app.use(json())
 app.use("/webhook", webhookRouter)
+app.use("/user", userRouter)
 
 app.get('/', (req: Request, res: Response) => {
   return res.send("gm")
--- src/routes/webhook.ts
+++ src/routes/webhook.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import axios from 'axios';
 import * as web3 from '@solana/web3.js';
 import dotenv from "dotenv"
+import { apiKeyProtection } from '../middlewares/apikey';
+import { DiscordWebhook } from '../types/discordWebhook';
 
 
 export const router = express.Router();
@@ -12,36 +14,22 @@ const connection = new web3.Connection(process.env.RPC_URL as string, {
   wsEndpoint: process.env.WS_URL,
 });
 
-router.post('/register', (req, res) => {
+router.post('/register', apiKeyProtection, (req, res) => {
   const { reference } = req.body
+
+  if (!reference) return res.send(400).json({ error: "Missing 'reference'" })
 
   const publicKey = new web3.PublicKey(reference)
 
   const id = connection.onAccountChange(publicKey, async (updatedAccountInfo, context) => {
     console.log("Updated account info: ", updatedAccountInfo)
-    axios.post("https://discord.com/api/webhooks/1102154954359177338/Ot35cV8vBytWkJvdN5CIY_VmDEk1eA1_nKHc0QgdfEcoxL-u189Sd-HdG-AUELR0Q44q",
-      {
-        username: "Change Made",
-        avatar_url: "",
-        content: reference,
-        embeds: [
-          {
-            "title": "Some title",
-            "color": 15258703,
-            "thumbnail": {
-              "url": "",
-            },
-            "fields": [
-              {
-                "name": "Your fields here",
-                "value": "Whatever you wish to send",
-                "inline": true
-              }
-            ]
-          }
-        ]
-      }
-    )
+
+    const data: DiscordWebhook = {
+      username: "Solana Pay",
+      content: updatedAccountInfo.toString()
+    }
+    axios.post("https://discord.com/api/webhooks/1102154954359177338/Ot35cV8vBytWkJvdN5CIY_VmDEk1eA1_nKHc0QgdfEcoxL-u189Sd-HdG-AUELR0Q44q", data)
+
     await connection.removeAccountChangeListener(id)
   },
     "confirmed"
